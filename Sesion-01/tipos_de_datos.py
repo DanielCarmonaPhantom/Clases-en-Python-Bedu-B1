924a30c2860534c052057839c840e534941b8d02
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-04-19 20:57:40 -0500

    Se creo el archivo de tipos de datos

---
+++ Sesion-01/tipos_de_datos.py
@@ -0,0 +1,18 @@
+# Tipos de datos numéricos
+entero = 4
+print( "El dato introducido contiene:", entero )
+print( "Es de tipo:", type(entero) )
+
+pi = 3.14159
+print("El dato introducido contiene:", pi)
+print("Es de tipo:", type(pi) )
+
+# Cadenas de texto
+mensaje = "Hola Mundo"
+print("El dato introducido contiene:", mensaje)
+print("Es de tipo:", type(mensaje) )
+
+# Datos booleanos
+verdadero = True
+print("El dato introducido contiene:", verdadero)
+print("Es de tipo:", type(verdadero) )
