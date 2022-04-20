e26d680a5aa244db05db405337c57fbe9460189c
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-04-19 21:28:47 -0500

    Se creo el archivo variables

---
+++ Sesion-01/variables.py
@@ -0,0 +1,7 @@
+num1 = int(input("Inserta número: "))
+num2 = int(input("Inserta otro número: "))
+
+suma = num1 + num2
+
+print(f"Suma numérica {suma}")
+print(f"Concatenación {str(num1) + str(num2)}")
