a2d67e2dcd290cc450a5ea65b211ee8fe8210e77
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-03 20:14:35 -0500

    Se termino el ejemplo 5 de docstrings

---
+++ Sesion-03/helado/__init__.py
@@ -0,0 +1,11 @@
+"""
+Nombre del paquete: helado
+
+Contiene los módulos organizados para realizar las distintas funciones para un sistema de helados.
+
+La lista de módulos disponibles:
+
+Entrada - Se encarga de ...
+pantalla - Se encarga de ...
+varios - Se encarga de ...
+"""
--- Sesion-03/helado/entrada.py
+++ Sesion-03/helado/entrada.py
@@ -1,3 +1,11 @@
+"""
+Nombre del módulo: entrada
+
+Contiene los elementos de apoyo para la lectura de valores.
+
+"""
+
+
 
 def lee_respuesta():
     """ Leer las opción elegida por el usuario """
