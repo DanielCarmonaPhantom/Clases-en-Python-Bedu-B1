274559d2178b21954c62f25eba0826999c08e031
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-10 15:49:18 -0500

    Se termino el ejemplo 1 de la sesion 5

---
+++ Sesion-05/main.py
@@ -0,0 +1,17 @@
+import persona
+
+
+def main():
+    # persona_1 = persona.Persona("Daniel")
+
+    # print(persona_1.get_email)
+
+    empleado_1 = persona.Empleado("Daniel")
+
+    # print(empleado_1)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ Sesion-05/persona.py
@@ -0,0 +1,66 @@
+class Persona():
+
+    def __init__(self, nombre, apellido=None, email = None):
+        self.nombre = nombre
+        self.apellido = apellido
+
+        self.__email = email
+
+    def asignar_nombre(self, nombre):
+        self.nombre = nombre
+
+    def cambiar_email(self, email):
+        self.__email = email
+
+    def saluda(self):
+        print("hola, soy {}".format(self.nombre))
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_email(self):
+        return self.__email
+
+    def __str__(self):
+        if self.apellido != None:
+            return f'{self.nombre} {self.apellido}'
+        else:
+            return f'{self.nombre}'
+    
+    def __add__(self, persona):
+        persona_nueva = Persona(self.nombre + ", " + persona.nombre)
+
+        return persona_nueva
+
+
+class Empleado (Persona):
+    def __init__(self, nombre, apellido = None, email: str= None, sueldo: float= 0.0):
+        super().__init__(nombre, apellido, email)
+        self.sueldo = sueldo
+
+    def put_sueldo(self, sueldo):
+        self.sueldo = sueldo
+
+    def __str__(self):
+        if self.apellido != None:
+            return f'{self.nombre} {self.apellido} | $ {self.sueldo} M.N.'
+        else:
+            return f'{self.nombre} | $ {self.sueldo} M.N.'
+        
+
+
+
+
+
+def main():
+    """
+    Función principal
+    """
+    empleado_1 = Empleado("Daniel")
+    print(empleado_1.saluda())
+    print(empleado_1.get_nombre())
+
+
+
+if __name__ == "__main__":
+    main()
