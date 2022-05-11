36dd4030890c5ba86887b60cc03e45f070627c61
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-10 20:50:35 -0500

    Se termino la conexion

--- Sesion-05/base_de_datos.py
+++ Sesion-05/base_de_datos.py
@@ -14,12 +14,20 @@ def sql_connection(db_name):
 
     return (conection, cur)
 
-def sql_table(con, cur, name):
+def sql_table_product(con, cur):
     """
     Crea una tabla con nombre en la base de datos
     """
-    sql = f"CREATE TABLE {name}"
+    sql = "CREATE TABLE employees(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)"
     cur.execute(sql)
+    con.commit()
+
+def sql_insert(con, cur, entities):
+    """
+    Inserta datos en una tabla
+    """
+    sql = 'INSERT INTO employees(id, name, salary, department, position, hireDate) VALUES(?, ?, ?, ?, ?, ?)'  
+    cur.execute(sql, entities)    
     con.commit()
 
 def main():
@@ -29,6 +37,9 @@ def main():
     con, cur = sql_connection(DB_NAME)
 
     print(con, cur)
+
+    print(sql_table_product(con, cur))
+
     cur.close()
     con.close()
 
