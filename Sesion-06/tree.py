9abdd2fa4056df9b61395278348db59a8419e21d
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-12 21:46:48 -0500

    Se termino el uso de agregar en csv

--- Sesion-06/tree.py
+++ Sesion-06/tree.py
@@ -1,6 +1,6 @@
-from email.policy import default
 import click
 import os
+import csv
 
 class Archivo():
     def __init__(self, ruta_nombre):
@@ -11,6 +11,12 @@ class Archivo():
         Representación en str de un objeto Archivo        
         """
         return self.ruta_nombre
+    
+    def list(self):
+        """
+        Representación en str de un objeto Archivo        
+        """
+        return [self.ruta_nombre,0,"2022-05-12"]
 
 class Carpeta(Archivo):
     def __init__(self, ruta_nombre):
@@ -56,20 +62,34 @@ def guarda_txt(elementos,archivo):
     with open(archivo, 'w') as f:
         for elemento in elementos:
             f.write(str(elemento) + "\n")
+def guarda_csv( elementos,archivo):
+    """
+    Guarda la lista de elementos en archivo en formato csv
+    """
+    with open(archivo, 'w') as f:
+        arch_write = csv.writer(f)
+        for elemento in elementos:
+            # fila =[str(elemento),0, "2022-08-12"]
+            arch_write.writerow(elemento.list())
+
+    print("Se ha creado el archivo salida.csv con los resultados")
 
 @click.command()
 @click.option("--txt", "archivo", type=str, help="Guarda el resultado en un archivo help")
+@click.option("--csv", "archcsv", type=str, help="Guarda el resultado en un archivo csv")
 @click.argument("carpeta", default=".", type=click.Path(exists=True))
-def main(archivo,carpeta):
+def main(archivo,archcsv,carpeta):
     """"
     Imprime la lista de carpetas y archivos, indicada en la línea de comandos. Si no se especifica, se utiliza la carpeta actual
     """
     carpeta_base = Carpeta(carpeta)
     elementos = carpeta_base.lista()
-    if archivo == None:
+    if archivo == None and archcsv == None:
         imprime_text_file(elementos)
-    else:
+    elif archivo != None:
         guarda_txt(elementos, archivo)
+    elif archcsv != None:
+        guarda_csv(elementos,archcsv)
 
 if __name__ == '__main__':
     main()
